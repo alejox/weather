c77bf3e8600699f302d46a4ab927734f51a29b48
alejox <alejoxdrums@gmail.com>
2022-05-10 00:31:28 -0500

    ADD

---
+++ src/components/Weather.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+
+const Weather = () => {
+
+    const [weather, setWheather] = useState({});
+
+      useEffect(()=>{
+
+        function success(pos) {
+          var crd = pos.coords;
+
+          console.log('Your current position is:');
+          console.log(`Latitude : ${crd.latitude}`);
+          console.log(`Longitude: ${crd.longitude}`);
+          console.log(`More or less ${crd.accuracy} meters.`);
+          axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=d200a8eb35acdb0332db3335d5175b2f`)
+            .then(res => setWheather(res.data));
+        }
+
+        function error(err) {
+          console.warn(`ERROR(${err.code}): ${err.message}`);
+        }
+
+        navigator.geolocation.getCurrentPosition(success, error);
+        
+    }, []);
+    
+    console.log(weather);
+
+        
+    return (
+        <div>
+            <h1>Weather App</h1>
+            <h3>{weather.name}, {weather.sys?.country}</h3>
+            <img src={`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}@2x.png`} alt="" />
+            <h3>{weather.weather?.[0].description}</h3>
+            <h3>Wind speed: {weather.wind?.speed}</h3>
+            <h3>Clouds: {weather.clouds?.all}%</h3>
+            <h3>Pressure: {weather.main?.pressure}</h3>
+            <h3>{weather.main?.temp} ºC</h3>
+            <button>Degrees ºF / ºC</button>
+            
+        </div>
+    );
+};
+
+export default Weather;
